5c742922c21c41cfee7a42883ad265a3fefa1cbb
NoGambling <Daniel.Apollonsky@gmail.com>
2024-07-12 19:49:49 -0400

    create checkers.py

---
+++ src/checkers.py
@@ -0,0 +1,15 @@
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+colorama_init()
+
+board = [" O "] * 16 + [""] * 32 + [" X "] * 16
+print(board)
+def createBoard(board):
+    boardToUse = board
+    for count in range(8):
+        for newCount in range(8):
+            print()
+
+
+createBoard(board)
