0f5f1b56233f765244ed1157fb591ed00643d501
NoGambling <Daniel.Apollonsky@gmail.com>
2024-07-12 21:23:28 -0400

    Finished createBoard command

--- src/checkers.py
+++ src/checkers.py
@@ -3,16 +3,18 @@ from colorama import Fore
 from colorama import Style
 colorama_init()
 
-board = [" O "] * 16 + ["   "] * 32 + [" X "] * 16
+board = [" O ", "  "] * 4 + ["  ", " O "] * 4 + [" O ", "  "] * 4 + ["  "] * 16 + ["  ", " X "] * 4 + [" X ", "  "] * 4 + ["  ", " X "] * 4
 
 def createBoard(board):
     boardToUse = board
     for count in range(8):
-        print(Fore.CYAN + "|",end="  " + Style.RESET_ALL)
+        print(Fore.CYAN + f"{count+1} |",end="  " + Style.RESET_ALL)
+
         for newCount in range(8):
-            
             spot = (count* 8) + newCount
             print("[" + board[spot] + "]",end=" ")
-        print("\n|  ")
-
+        
+        print("\n  |  ")
+    print("   ----------------------------------------------")
+    print("      A     B    C     D     E    F    G     H")
 createBoard(board)  
