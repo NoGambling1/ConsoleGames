f49e190d1f886d9246e2bed64b281c39b2f98b98
NoGambling <Daniel.Apollonsky@gmail.com>
2024-07-13 22:45:16 -0400

    started more functions

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,4 +1,7 @@
-board = [" E "] * 16 + [""] * 6
+import random
+
+global board 
+board = ["   "] * 16
 
 class color:
    PURPLE = '\033[95m'
@@ -13,7 +16,6 @@ class color:
    END = '\033[0m'
 
 def createBoard(board):
-    #print("----------------------------------------------")
     for a in range(4):
         if a == 2:
             print(color.BOLD + color.CYAN +"---------------------" + color.END)
@@ -26,9 +28,30 @@ def createBoard(board):
             elif thingy > 16: print(" |")
             elif i == 0: print("|" + board[thingy],end=" |")
             else: print(board[thingy],end=" |") # type: ignore
-    
+        
         print("")
     print("---------------------")
-createBoard(board)
 
-    
+def randomizeBoard(difficulty): #Diff 1-3,  1=40%, 2=32%, 3=25%
+    if difficulty == 1: given = 40
+    elif difficulty == 2: given = 32
+    elif difficulty == 3: given = 25
+
+    for i in range(16):
+        randie = random.randrange(100) < given
+        if randie:
+            board[i] = (" " + str(random.randint(1, 4)) + " ")
+    return board
+
+print(board[all()])
+def checkSpot(spot)
+    spot = spot
+    #if (int(spot) - 4 == spot or int(spot) + 4 == spot)
+    if spot < 4:
+        if board[spot+4] != spot and board[spot+8] != spot and board[spot+12] != spot:
+            print("hi")
+    elif spot < 8:
+    elif spot < 12:
+    else:
+print(randomizeBoard(1))
+createBoard(randomizeBoard(1))
