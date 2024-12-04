2f9c17581be02a44f3e6ebf3f7615e7c4f0e4a9f
LL-LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2024-12-04 09:45:38 -0500

    typo

--- src/gamblingandorangesconsolegames/system_info.py
+++ src/gamblingandorangesconsolegames/system_info.py
@@ -6,7 +6,7 @@ import os
 def get_system_info():
     info = {
         "status: updating",
-        "OS": f"{platform.system()} {platform.release()}",
+        "OS": "{platform.system()} {platform.release()}",
         "PY version": platform.python_version(),
         "processor": platform.processor(),
         "machine": platform.machine(),
